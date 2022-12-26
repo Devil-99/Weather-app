761b4c46d9c1695cc55ed553dad4c58202a55ce4
Devil-99 <souvikmaji1999@gmail.com>
2022-12-26 12:55:05 +0530

    modified

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ const http=require('http');
 const fs=require('fs');
 var requests=require('requests');
 
-const homeFile=fs.readFileSync('index.html','utf-8');
+var homeFile;
+try{
+homeFile = fs.readFileSync('./landingPage.html','utf-8');}
+catch(err){
+    console.log(err);
+}
 
 const replaceVal =(prev,pres)=>{
     let temperature=prev.replace("{%tempval%}",pres.main.temp);
@@ -15,20 +20,22 @@ const replaceVal =(prev,pres)=>{
     temperature=temperature.replace("{%tempmax%}",pres.main.temp_max);
     temperature=temperature.replace("{%wind%}",pres.wind.speed);
 
-    //console.log(pres.weather[0].main);
     return temperature;
+}
+
+function weatherapi(city){
+    return "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=4580466da90099b152f2a5bf0ec183c1";
 }
 
 const server = http.createServer((req,res)=>{
     if(req.url=='/')
     {
-        requests("http://api.openweathermap.org/data/2.5/weather?q=Bankura&units=metric&appid=4580466da90099b152f2a5bf0ec183c1")
+        let url = weatherapi("bankura");
+        requests(url)
         .on("data",(chunk)=>{
             const objData=JSON.parse(chunk);
-            const arrData=[objData];
-            const realtimeData = arrData.map((val)=>replaceVal(homeFile,val)).join("");
+            const realtimeData = replaceVal(homeFile,objData);
             res.write(realtimeData);
-            // console.log(objData);
         })
         .on("end",(err)=>{
             if(err)
